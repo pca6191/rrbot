aec7b69a3d100b647e03849cf0802330040088e1
Kc Chang 張耿智 (中光電) <kc.chang@coretronic.com>
2020-09-21 09:15:17 +0800

    [qt_app] 新增 qt5 gui 樣板 package

---
+++ qt_app/README.md
@@ -0,0 +1,8 @@
+## 說明
+
+這是一個 qt app, 利用 qt5 相關 dependency.
+
+## Ref:
+https://blog.csdn.net/lingchen2348/article/details/82344924
+
+KC, 2020-09-20
---
+++ qt_app/src/qt_app_node.cpp
@@ -0,0 +1,13 @@
+// #include <qt_app_node.h>
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
